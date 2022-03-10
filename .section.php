9679f2d58769cbf08e589a499732f739eddbe271
alisherrusinov <alisher.nugmanoff@gmail.com>
2022-03-10 11:44:52 +0500

    init commit

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Три совы',
+	'description' => 'Description',
+	'keywords' => 'Keywords',
+	'robots' => 'index, follow'
+);
+?>
---
+++ bitrix/templates/three_owl/description.php
@@ -0,0 +1,9 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+
+$arTemplate = Array(
+	'NAME' => 'Три совы',
+	'DESCRIPTION' => 'Шаблон сайта Три совы'
+);
+?>
