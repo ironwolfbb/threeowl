6666a8c239bc34cfcc24b34a05981d584a064535
alisherrusinov <alisher.nugmanoff@gmail.com>
2022-03-11 12:51:42 +0500

    added top nav menu

---
+++ .left.menu.php
@@ -0,0 +1,32 @@
+<?
+$aMenuLinks = Array(
+    Array(
+        "О бренде",
+        "/about/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Где купить?",
+        "/buy/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Блог",
+        "/blog/",
+        Array(),
+        Array(),
+        ""
+    ),
+    Array(
+        "Контакты",
+        "/contacts/",
+        Array(),
+        Array(),
+        ""
+    ),
+);
+?>
