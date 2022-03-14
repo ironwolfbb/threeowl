16e2be6f0a40c239de5fd8b9d589cf8737255eb5
alisherrusinov <alisher.nugmanoff@gmail.com>
2022-03-14 14:39:19 +0500

    added novelry slider

--- index.php
+++ index.php
@@ -83,94 +83,69 @@ $APPLICATION->SetTitle('Главная');
                     Наши новинки
                 </h2>
             </div>
-            <div class="js-second-mini-slider smaller-slider swiper-container second-mini-slider">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <a href="#"></a>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/game.jpg" alt="">
-                        <span>
-                            Артикул: 256787
-                        </span>
-                        <p>
-                            Набор развивающий Росигрушка Ассорти 21 кубик +  пирамидка 6146
-                        </p>
-                    </div>
-                    <div class="swiper-slide">
-                        <a href="#"></a>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/game.jpg" alt="">
-                        <span>
-                            Артикул: 256787
-                        </span>
-                        <p>
-                            Набор развивающий Росигрушка Ассорти 21 кубик +  пирамидка 6146
-                        </p>
-                    </div>
-                    <div class="swiper-slide">
-                        <a href="#"></a>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/game.jpg" alt="">
-                        <span>
-                            Артикул: 256787
-                        </span>
-                        <p>
-                            Набор развивающий Росигрушка Ассорти 21 кубик +  пирамидка 6146
-                        </p>
-                    </div>
-                    <div class="swiper-slide">
-                        <a href="#"></a>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/game.jpg" alt="">
-                        <span>
-                            Артикул: 256787
-                        </span>
-                        <p>
-                            Набор развивающий Росигрушка Ассорти 21 кубик +  пирамидка 6146
-                        </p>
-                    </div>
-                    <div class="swiper-slide">
-                        <a href="#"></a>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/game.jpg" alt="">
-                        <span>
-                            Артикул: 256787
-                        </span>
-                        <p>
-                            Набор развивающий Росигрушка Ассорти 21 кубик +  пирамидка 6146
-                        </p>
-                    </div>
-                    <div class="swiper-slide">
-                        <a href="#"></a>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/game.jpg" alt="">
-                        <span>
-                            Артикул: 256787
-                        </span>
-                        <p>
-                            Набор развивающий Росигрушка Ассорти 21 кубик +  пирамидка 6146
-                        </p>
-                    </div>
-                    <div class="swiper-slide">
-                        <a href="#"></a>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/game.jpg" alt="">
-                        <span>
-                            Артикул: 256787
-                        </span>
-                        <p>
-                            Набор развивающий Росигрушка Ассорти 21 кубик +  пирамидка 6146
-                        </p>
-                    </div>
-                </div>
-
-                <div class="slider-arrow prev-mini-slide prev-slide">
-                    <svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="45" height="44.8462" rx="22.4231" fill="white" />
-                        <path d="M25 14.9487L19 22.4231L25 29.8974" stroke="#FFCF32" stroke-width="2" />
-                    </svg>
-                </div>
-                <div class="slider-arrow next-mini-slide next-slide">
-                    <svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="45" height="45" rx="22.5" fill="white" />
-                        <path d="M20 15L26 22.5L20 30" stroke="#FFCF32" stroke-width="2" />
-                    </svg>
-                </div>
-                <div class="swiper-pagination second-slider-pagination"></div>
-            </div>
+            <?$GLOBALS['NoveltyFilter'] = array('PROPERTY_is_novelty'=>1);?>
+            <?$APPLICATION->IncludeComponent("bitrix:news.list", "novelty-list", Array(
+                "ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+                "ADD_SECTIONS_CHAIN" => "Y",	// Включать раздел в цепочку навигации
+                "AJAX_MODE" => "N",	// Включить режим AJAX
+                "AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+                "AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+                "AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+                "AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
+                "CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+                "CACHE_GROUPS" => "Y",	// Учитывать права доступа
+                "CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+                "CACHE_TYPE" => "A",	// Тип кеширования
+                "CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
+                "DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
+                "DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
+                "DISPLAY_DATE" => "Y",	// Выводить дату элемента
+                "DISPLAY_NAME" => "Y",	// Выводить название элемента
+                "DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
+                "DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
+                "DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+                "FIELD_CODE" => array(	// Поля
+                    0 => "",
+                    1 => "",
+                ),
+                "FILTER_NAME" => "NoveltyFilter",	// Фильтр
+                "HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
+                "IBLOCK_ID" => "1",	// Код информационного блока
+                "IBLOCK_TYPE" => "catalog",	// Тип информационного блока (используется только для проверки)
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",	// Включать инфоблок в цепочку навигации
+                "INCLUDE_SUBSECTIONS" => "Y",	// Показывать элементы подразделов раздела
+                "MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+                "NEWS_COUNT" => "20",	// Количество новостей на странице
+                "PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+                "PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+                "PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+                "PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+                "PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
+                "PAGER_TITLE" => "Новости",	// Название категорий
+                "PARENT_SECTION" => "",	// ID раздела
+                "PARENT_SECTION_CODE" => "",	// Код раздела
+                "PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
+                "PROPERTY_CODE" => array(	// Свойства
+                    0 => "",
+                    1 => "",
+                ),
+                "SET_BROWSER_TITLE" => "Y",	// Устанавливать заголовок окна браузера
+                "SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+                "SET_META_DESCRIPTION" => "Y",	// Устанавливать описание страницы
+                "SET_META_KEYWORDS" => "Y",	// Устанавливать ключевые слова страницы
+                "SET_STATUS_404" => "N",	// Устанавливать статус 404
+                "SET_TITLE" => "Y",	// Устанавливать заголовок страницы
+                "SHOW_404" => "N",	// Показ специальной страницы
+                "SORT_BY1" => "ACTIVE_FROM",	// Поле для первой сортировки новостей
+                "SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
+                "SORT_ORDER1" => "DESC",	// Направление для первой сортировки новостей
+                "SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
+                "STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
+                "COMPONENT_TEMPLATE" => ".default"
+            ),
+                false
+            );?>
 
         </div>
     </div>
---
+++ local/templates/three_owl/components/bitrix/news.list/novelty-list/template.php
@@ -0,0 +1,53 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="js-second-mini-slider smaller-slider swiper-container second-mini-slider">
+    <div class="swiper-wrapper">
+        <?foreach($arResult["ITEMS"] as $arItem):?>
+        <?
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+        ?>
+        <div class="swiper-slide">
+            <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"></a>
+            <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"/>
+            <span>
+                            Артикул: 256787
+                        </span>
+            <p>
+                <?=$arItem['NAME']?>
+            </p>
+        </div>
+        <?endforeach;?>
+    </div>
+
+    <div class="slider-arrow prev-mini-slide prev-slide">
+        <svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="45" height="44.8462" rx="22.4231" fill="white" />
+            <path d="M25 14.9487L19 22.4231L25 29.8974" stroke="#FFCF32" stroke-width="2" />
+        </svg>
+    </div>
+    <div class="slider-arrow next-mini-slide next-slide">
+        <svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="45" height="45" rx="22.5" fill="white" />
+            <path d="M20 15L26 22.5L20 30" stroke="#FFCF32" stroke-width="2" />
+        </svg>
+    </div>
+    <div class="swiper-pagination second-slider-pagination"></div>
+</div>
+
+
+
+
+
