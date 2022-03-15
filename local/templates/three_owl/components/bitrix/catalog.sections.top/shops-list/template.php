4f44780d7c1d765b6449dd84bd9158f7560ff6c6
alisherrusinov <alisher.nugmanoff@gmail.com>
2022-03-15 11:45:17 +0500

    shops list

---
+++ local/templates/three_owl/components/bitrix/catalog.sections.top/shops-list/template.php
@@ -0,0 +1,38 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+<?foreach($arResult["SECTIONS"] as $arSection):?>
+<div class="buying-links">
+    <h3>
+        <?=$arSection["NAME"]?>
+    </h3>
+    <div class="buying-links-item">
+    <?
+    $cell = 0;
+    foreach($arSection["ITEMS"] as $arElement):?>
+        <?
+        $this->AddEditAction($arElement['ID'], $arElement['EDIT_LINK'], CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_EDIT"));
+        $this->AddDeleteAction($arElement['ID'], $arElement['DELETE_LINK'], CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BCST_ELEMENT_DELETE_CONFIRM')));
+        ?>
+        <a href="<?=$arElement["DETAIL_PAGE_URL"]?>">
+            <img src="<?=$arElement["PREVIEW_PICTURE"]["SRC"]?>">
+        </a>
+    <?endforeach;?>
+        </div>
+</div>
+<?endforeach;?>
+
+
+
