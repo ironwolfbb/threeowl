40fd79352e91a084a6b142768b96e53076e5af10
alisherrusinov <alisher.nugmanoff@gmail.com>
2022-05-12 09:32:48 +0500

    detail page bread fix

--- catalog/index.php
+++ catalog/index.php
@@ -5,7 +5,7 @@ $APPLICATION->SetTitle("Каталог");
 	"bitrix:news", 
 	"catalog", 
 	array(
-		"ADD_ELEMENT_CHAIN" => "N",
+		"ADD_ELEMENT_CHAIN" => "Y",
 		"ADD_SECTIONS_CHAIN" => "Y",
 		"AJAX_MODE" => "N",
 		"AJAX_OPTION_ADDITIONAL" => "",
