95a57b61768657ec15dd6708ca5b2373d29f9461
ironwolf <ironwolf.bb@gmail.com>
2022-07-29 19:04:31 +0500

    8.5

--- local/templates/three_owl/components/bitrix/form.result.new/blog-mailing-form/template.php
+++ local/templates/three_owl/components/bitrix/form.result.new/blog-mailing-form/template.php
@@ -10,11 +10,15 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
             Подписаться на блог
         </p>
         <label>
-            <?if ($arResult["isFormErrors"] == "Y"):?><?=$arResult["FORM_ERRORS_TEXT"];?><?endif;?>
-            <?= $arResult["QUESTIONS"]['email_field_blog']['HTML_CODE'] ?>
+            <? if ($arResult["isFormErrors"] == "Y") {
+            echo str_replace("name", "class='error' name", $arResult["QUESTIONS"]['email_field_blog']['HTML_CODE']);
+        } else {
+            echo $arResult["QUESTIONS"]['email_field_blog']['HTML_CODE'];
+        }
+         endif; ?>
         </label>
         <div class="btn btn-orange submit-btn">
             <button type="submit" class="btn btn-more submit-btn" name="web_form_submit" value="<?= $arResult["arForm"]["BUTTON"]?>">Отправить</button>
         </div>
     <?= $arResult["FORM_FOOTER"] ?>
-<? endif; ?>
+
