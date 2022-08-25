1461b7bb5e92f3d0fc6f73835f097a9433173ce8
ironwolf <ironwolf.bb@gmail.com>
2022-08-25 22:20:37 +0500

    mailing form

--- local/templates/three_owl/components/bitrix/form.result.new/mailing-form/template.php
+++ local/templates/three_owl/components/bitrix/form.result.new/mailing-form/template.php
@@ -23,8 +23,9 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
             </div>
         </div>
         <label>
-
-        <? if ($arResult["isFormErrors"] == "Y") {
+           <? $arResult["QUESTIONS"]['email_field']['HTML_CODE'] = str_replace('type="text"', 'type="email"', $arResult["QUESTIONS"]['email_field']['HTML_CODE'])
+?>
+        <? if ($arResult["isFormErrors"] == "Y" && $arResult["arrVALUES"]['form_email_1'] == '') {
             echo str_replace("name", "class='error' name", $arResult["QUESTIONS"]['email_field']['HTML_CODE']);
         } else {
             echo $arResult["QUESTIONS"]['email_field']['HTML_CODE'];
@@ -32,7 +33,49 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
     endif; ?>
         </label>
         <div class="btn btn-orange subscribe">
-            <input class="btn btn-more" type="submit" name="web_form_submit" value="<?= $arResult["arForm"]["BUTTON"] ?>">
+            <input class="btn btn-more" type="submit" name="web_form_submit"  value="<?= $arResult["arForm"]["BUTTON"] ?>">
         </div>
     </div>
+   
     <?= $arResult["FORM_FOOTER"] ?>
+
+   <!--  <script>
+    const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+const input = document.querySelector('input');
+
+function isEmailValid(value) {
+ 	return EMAIL_REGEXP.test(value);
+}
+
+function onInput() {
+	if (isEmailValid(input.value)) {
+		input.style.borderColor = 'green';
+	} else {
+		input.style.borderColor = 'red';
+	}
+}
+
+input.addEventListener('input', onInput);
+
+  </script> -->
+
+<script>
+    // Записываем в переменую эту форму
+let elementForm = document.getElementsByName("mailing_form")[0]
+// Вешаем событие которое будет слушать отправку, код ниже будет работать только по нажатию кнопки отправить
+elementForm.addEventListener("submit", function(e){
+    let isNeedToPrevent = false;
+    
+let inputMail = document.getElementsByName("form_email_1")[0].value; // form_email_1 name который у инпута
+  if(inputMail == ''){
+    isNeedToPrevent = true;
+    document.getElementsByName("form_email_1")[0].classList.add('error');
+  }else{
+    document.getElementsByName("form_email_1")[0].classList.remove('error');
+  }
+  if(isNeedToPrevent){
+    e.preventDefault();
+  }
+});
+  </script>
+ 
