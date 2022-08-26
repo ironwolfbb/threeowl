8eb90084d6aeb5419d2247be1aab7acb052b4a9c
ironwolf <ironwolf.bb@gmail.com>
2022-08-26 15:17:52 +0500

    blog-miling-form

--- local/templates/three_owl/components/bitrix/form.result.new/blog-mailing-form/template.php
+++ local/templates/three_owl/components/bitrix/form.result.new/blog-mailing-form/template.php
@@ -10,6 +10,8 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
             Подписаться на блог
         </p>
         <label>
+        <? $arResult["QUESTIONS"]['email_field_blog']['HTML_CODE'] = str_replace('type="text"', 'type="email"', $arResult["QUESTIONS"]['email_field_blog']['HTML_CODE'])
+?>
             <? if ($arResult["isFormErrors"] == "Y") {
             echo str_replace("name", "class='error' name", $arResult["QUESTIONS"]['email_field_blog']['HTML_CODE']);
         } else {
@@ -21,4 +23,24 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
             <button type="submit" class="btn btn-more submit-btn" name="web_form_submit" value="<?= $arResult["arForm"]["BUTTON"]?>">Отправить</button>
         </div>
     <?= $arResult["FORM_FOOTER"] ?>
+
+    <script>
+    // Записываем в переменую эту форму
+let elementForm = document.getElementsByName("blog_mailing_form")[0]
+// Вешаем событие которое будет слушать отправку, код ниже будет работать только по нажатию кнопки отправить
+elementForm.addEventListener("submit", function(e){
+    let isNeedToPrevent = false;
+    
+let inputMail = document.getElementsByName("form_email_3")[0].value; // form_email_1 name который у инпута
+  if(inputMail == ''){
+    isNeedToPrevent = true;
+    document.getElementsByName("form_email_3")[0].classList.add('error');
+  }else{
+    document.getElementsByName("form_email_3")[0].classList.remove('error');
+  }
+  if(isNeedToPrevent){
+    e.preventDefault();
+  }
+});
+  </script>
 
